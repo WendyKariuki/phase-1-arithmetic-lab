cf00a26085bc4cf88c8fc0a4d16b85ff7c62d196
WendyKariuki <phaminuswendy@gmail.com>
2024-03-19 22:33:25 +0300

    JS lab4

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+var num1 = 31
 
-// Write your code here
+var num2 = 2
+
+ var multiply = num1 * num2;
+
+console.log(multiply)
+
+var random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+console.log(random);
+
+
+ var num3 = 16
+ var num4 = 6
+ var mod = num3 % num4;
+
+console.log(mod)
+
+var numbers = [10, 20, 5];
+
+var max = Math.max(...numbers)
+
+console.log(max)
